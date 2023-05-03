769c8dde659987f0b3699c4086a32db807ee5fc0
Haave ( Gleison ) <gleisonsenac06@gmail.com>
2023-05-03 18:03:56 -0300

    Feature002
    Crie um programa que receba trÊS vakires. Imprima a soma dos dois primeiros multiplicada pelo terceiro.

---
+++ Exercicio002.py
@@ -0,0 +1,10 @@
+
+#Crie um programa que receba trÊS vakires. Imprima a soma dos dois primeiros multiplicada pelo terceiro.
+
+valor = 5
+valor2 = 10
+valor3 = 3
+
+total= (valor+valor2) * valor3 
+
+print ("O valor de",valor, "+", valor2, "=", total, sep=" ")
