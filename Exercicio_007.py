f7888383929ceb25afd45b05ec36fae5aa9140a5
Haave ( Gleison ) <gleisonsenac06@gmail.com>
2023-05-05 17:50:50 -0300

    Feature_008
    Transformandor de Fahrenheit para Celsius.

--- Exercicio_007.py
+++ Exercicio_007.py
@@ -3,3 +3,16 @@
 a = "Gleison"
 b = "Morais"
 print ("Prezado "+a+" "+b+"" ". Olá!")
+
+#Replicação de Strings
+
+print (2*"Gleison\n") #pedir para escrever a string com determinado numero de vezes
+
+print ("+" + 10 *"-" + "+")
+print (("|" + " "*10 + "|\n") * 5, end="")
+print ("+" + 10 *"-" + "+")
+
+nome = input("Qual seu nome: ")
+valor = int(input("Quantas veses você deseja repetir esse nome? "))
+
+print (valor* ("%s \n" %(nome)))
