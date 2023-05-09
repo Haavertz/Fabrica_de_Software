1f8f171885afb3f0e78f2ed7e4c5daf9a0337f8f
Haave ( Gleison ) <gleisonsenac06@gmail.com>
2023-05-09 15:15:31 -0300

    Manipulação da String.py

---
+++ Exercicio_010 ( Manipulação da String.py).py
@@ -0,0 +1,20 @@
+a = "Gleison Morais"
+b = "gleissin"
+s = "Hello World!"
+
+
+print(len(a))
+print(a.capitalize()) #Deixa a Primeira letra Maiúscula
+print(a.upper()) #Deixa tudo Maiúsculo
+print(a.casefold()) #Deixa Minúsculo
+print(a.islower()) #Perguntação se a variável é Maiúscula
+print(a.isupper()) #Perguntação se a variável é Minúscula
+print(b.isdigit()) #Vê se tem digito
+print(a.replace("Gleison","fon")) #Substitui a Variável
+print(a.find("Gleison")) 
+print(a.rfind("a"))
+print("Gleison"in a)
+print(a.count("a"))
+print(s[-3]) #Caracteres direita pra esquerda 
+print(s[1:3]) #Do um ao terceiro
+print(s[:5]) #Do começo ao final
