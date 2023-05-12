c4a09d4242c5118bc0292860e62ee047075e31b3
Haave ( Gleison ) <gleisonsenac06@gmail.com>
2023-05-12 17:07:53 -0300

    feature List

--- LIST - PY/Exercicio_016.py
+++ LIST - PY/Exercicio_016.py
@@ -7,3 +7,18 @@ a = [1,2,3,4,5,6,7,8,9,10]
 print (min(a))
 print (max(a))
 print (sum(a))
+
+lista = ['a', 'b', 'c', 'd', 'e', 'f']
+print (lista [1:3])
+print (lista [:4])
+print (lista [3:])
+print (lista [:])
+print (lista [0:6])
+
+#Com isto eu consigo alterar strings dentro da lista, difentemente da string normal.
+
+frutas = ["banana", "maça", "cereja"]
+print (frutas)
+frutas [0] = "pera"
+frutas [-1] = "laranja"
+print (frutas)
