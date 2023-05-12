e1c1b594efeee2d2ea99165a622f593139be135f
Haave ( Gleison ) <gleisonsenac06@gmail.com>
2023-05-12 15:33:41 -0300

    feature

---
+++ Exercicio_014.py
@@ -0,0 +1,17 @@
+#Calcule e mostre: 
+ # O produto do dobro do primeiro com metade do segundo.
+ # A soma do triplo do primeiro com o terceiro.
+ # O terceiro elevado ao cubo.
+
+
+a = float(input("Qual o primeiro numero? "))
+b = float(input("Qual o primeiro numero? "))
+c = float(input("Qual o primeiro numero? "))
+
+a2 = (a*2) + (b/2)
+b2 = (a*3) + c
+c3 = c**3
+
+print ("1 - %.2f" % a2)
+print ("2 - %.2f" % b2)
+print ("3 - %.2f" % c3)
