d51a7b922c523777ad6510c94817026b63f3883f
Haave ( Gleison ) <gleisonsenac06@gmail.com>
2023-05-12 16:47:11 -0300

    feature00[15/16]

---
+++ LIST - PY/Exercicio_015.py
@@ -0,0 +1,31 @@
+
+#Vetor - LIST []
+
+b = ["Python", 3.5, True, 1]
+lista = [10,20,30,40, ["string",2]]
+print (lista)
+print(type(lista))
+
+print (b)
+print (type(b))
+
+#LEN
+
+b2 = ["Python", 3.5, True, 1]
+lista2 = [10,20,30,40, ["string",2]]
+print (len(lista2))
+print (len(b2))
+
+#Testes...
+
+uma_lista = [3,67,"gato", [56,57,"cachorro"],[],]
+print (uma_lista[3][1])
+
+#IN and Not in 
+#IN = pergutando se tem "Gato" na variavel animais
+#Not IN = pergutando se não e verdadeiro na variavel animais
+
+animais = ["Gato", "Cachorro"]
+print ("Gato" in animais)
+print ("Cachorro" not in animais)
+
