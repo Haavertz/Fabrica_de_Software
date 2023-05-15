5db395c01b823f1509aef6e3405efb4a35585313
Haave ( Gleison ) <gleisonsenac06@gmail.com>
2023-05-15 16:57:08 -0300

    feature_update

---
+++ IF - PY (Estrutura de Se[Condição])/Exercicio_019.py
@@ -0,0 +1,15 @@
+idade = 18
+
+if idade <= 20:
+    print ("Você é jovem!") #Já feito no portugol IF = Se
+else:   
+    print ("Você esta velho :(")#Já feito no portugol ELSE = Senao
+
+
+idade = int(input("Qual a sua idade? "))
+
+if idade <= 20:
+    print ("Voce é jovem!")
+else:
+    print ("Você é velho!")
+
---
+++ IF - PY (Estrutura de Se[Condição])/Exercicio_020.py
@@ -0,0 +1,7 @@
+number1 = int(input("Qual o primeiro numero: "))
+number2 = int(input("Qual o segundo numero: "))
+
+if number1 > number2:
+    print (number1)
+else:
+    print (number2)
