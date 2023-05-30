41bb838c131011c88a9ea5146bae95ebebbbba49
GleisonSenac <130395809+Haavertz@users.noreply.github.com>
2023-05-30 15:10:46 -0300

    Delete Exercicio_038.py

--- FOR - PY/Exercicio_038.py
+++
@@ -1,8 +0,0 @@
-vogal = input("Digite APENAS, UMA palavra:")
-
-if vogal == "A" or vogal == "E" or vogal == "I" or vogal == "O" or vogal == "U":
-    print ("Sua palavra É uma vogal")
-elif vogal == "a" or vogal == "e" or vogal == "i" or vogal == "o" or vogal == "u":
-     print ("Sua palavra É uma vogal")
-else:
-    print ("Sua palavra NÃO e uma vogal")
