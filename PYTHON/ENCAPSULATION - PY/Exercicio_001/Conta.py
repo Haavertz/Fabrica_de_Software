537c0c54833d6ac419f619388dcabb1b393cc1b6
Gleison <gleisonsenac06@gmail.com>
2023-07-13 13:08:24 -0300

    Exercicio_001
    encapsulation ainda não terminado

---
+++ PYTHON/ENCAPSULATION - PY/Exercicio_001/Conta.py
@@ -0,0 +1,25 @@
+class Conta:
+    def __init__(self, nome, cpf, saldo, senha):
+        self.nome = nome
+        self.__cpf = cpf
+        self.__saldo = saldo
+        self.__senha = senha
+        
+    def depositar(self, newvalor):
+        self.__newvalor = newvalor
+        self.__saldo = self.__saldo + self.__newvalor
+        return self.__saldo
+
+    def extrato(self, senha):
+        if senha == self.__senha:
+            print(f"O seu saldo é {self.saldo}")
+            return
+        else:
+            print("Senha Invalido!")
+
+    def sacar(self, senha):
+        if senha == self.__senha:
+            print(f"O seu saldo é: {self.__saldo}")
+            return
+        else:
+            print("Senha Invalida")
---
+++ PYTHON/ENCAPSULATION - PY/Exercicio_001/ContaTesting.py
@@ -0,0 +1,6 @@
+from Conta import *
+
+c1 = Conta("Gleison",1, 1200, "gleison")
+
+c1.depositar(20)
+c1.sacar("gleison")
