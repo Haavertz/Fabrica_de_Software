25aae1a90ecc331d9fd7acfd1ea9fad48eef4758
Gleison <gleisonsenac06@gmail.com>
2023-07-13 13:10:08 -0300

    Exercicio_049
    arrumando o dicionario

--- PYTHON/DICT - PY/Exercicio049.py
+++ PYTHON/DICT - PY/Exercicio049.py
@@ -6,20 +6,32 @@ print (dicio)'''
 
 '''print ('==========================================================================================================================================')'''
 
-'''semana = {}
-semana = {'domingo':[],'segunda' : 'Ederson', 'terça' : 'Mauricio','Quarta' : 'Ederson','Quinta' : 'Mauricio','Sexta' : 'Ederson','Sábado' : []}
-print (semana)'''
+semana = {}
+semana = {
+          'segunda' : 'Ederson', 
+          'terça' : 'Mauricio',
+          'Quarta' : 'Ederson',
+          'Quinta' : 'Mauricio',
+          'Sexta' : 'Ederson',
+          'Sábado' : [], 
+          'domingo':[]
+          }
+print (semana)
 
 '''print ('==========================================================================================================================================')'''
 agenda = {}
-while True:
-    cadastro = int(input("dada:"))
+cadastro = 1
+while cadastro != 0:
+    print("( 1 ) - Cadastro \n ( 0 ) - Sair/Mostrar \n")
+    cadastro = int(input("Escolha: "))
     if cadastro == 1:
         nome = input("Digite o nome para cadastrar no cpf: ")
         idade = input("Digite sua idade para cadastrar no cpf: ")
         telefone = input("Digite seu telefone para cadastrar no cpf: ")
-        cpf = input("Digite seu telefone para cadastrar no cpf: ")
-        agenda[cpf]={'nome':nome, 'idade':idade, 'telefone':telefone}
-    elif cadastro == 0:
-        break
+        cpf = input("Digite o numero do cpf: ")
+        agenda[cpf]={
+            'nome':nome, 
+            'idade':idade, 
+            'telefone':telefone}
+
 print (agenda)
