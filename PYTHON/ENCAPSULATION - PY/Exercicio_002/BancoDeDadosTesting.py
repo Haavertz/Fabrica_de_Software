8c633de2a845e509da2e4cc22894695ece7fd7c7
Gleison <gleisonsenac06@gmail.com>
2023-07-14 17:27:47 -0400

    Herança/BancoDeDados

--- PYTHON/ENCAPSULATION - PY/Exercicio_002/BancoDeDadosTesting.py
+++ PYTHON/ENCAPSULATION - PY/Exercicio_002/BancoDeDadosTesting.py
@@ -4,11 +4,11 @@ from BancoDeDados import *
 p1 = BaseDeDados()
 
 p1.inserirCliente(1, "Gleison")
-# p1.listar_dados()
+p1.listar_dados()
 p1.inserirCliente(2, "Otavio")
-# p1.listar_dados()
+p1.listar_dados()
 p1.inserirCliente(3, "Larissa")
-#p1.listar_dados()
+p1.listar_dados()
 
 
 
---
+++ PYTHON/HERANÇA - PY/Exercicio_001/PessoaTesting.py
@@ -0,0 +1,5 @@
+from Pessoa import *
+
+c1 = Pessoa("Gleison", 17, "Rua abadia Jabuir 1210", "Ceara", "CG")
+
+c2 = Aluno("Gleison", 17, "Rua abadia Jabuir 1210", "Ceara", "CG", 1000)
