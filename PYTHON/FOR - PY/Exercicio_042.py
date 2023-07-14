53fb7e940c10673f4fdf6a46ff2dd66f28fd420f
Gleison <gleisonsenac06@gmail.com>
2023-07-14 00:45:46 -0300

    TrocaDePast

--- PYTHON/FOR - PY/Exercicio_042.py
+++ PYTHON/FOR - PY/Exercicio_042.py
@@ -1,7 +1,7 @@
 lista = []
 lista2 = []
 item = 0
-for j in range(3,0):
+for j in range(5,0):
     lista.append(input("Digite um número inteiro: "))
     for item in lista:
         print (item*item)
@@ -9,7 +9,7 @@ for j in range(3,0):
 for n in range(50,101,2):
     print(n)
 
-for j in range(3):
+for j in range(5):
     lista.append(input("Digite um número inteiro: "))
     print("Maior número: ", max(lista))
     print("Menor número: ", min(lista))
