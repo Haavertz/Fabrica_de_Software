5e9248db56d249f23d2d9c2403b03079cb6d5efe
Gleison <gleisonsenac06@gmail.com>
2023-07-19 18:52:36 -0300

    testeParaForca

---
+++ PYTHON/dnaia.py
@@ -0,0 +1,11 @@
+palavra = "boa"
+palavra = list(palavra.upper())
+# print (palavra)
+kraio = "b"
+kraio = kraio.upper()
+li = "o".upper()
+if kraio == li:
+    print("palavra ja dita")
+else:
+    if kraio in palavra:  
+        print(kraio)
