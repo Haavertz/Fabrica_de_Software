afdaf21009a288255b22878471518ed99f532994
Gleison <gleisonsenac06@gmail.com>
2023-10-05 00:41:26 -0300

    Update004

--- PYSIDE/EXERCICIOS - PY/Exercicio_004.py
+++ PYSIDE/EXERCICIOS - PY/Exercicio_004.py
@@ -1,4 +1,4 @@
-from PySide6.QtWidgets import QApplication, QWidget, QPushButton, QMainWindow, QLabel
+from PySide6.QtWidgets import QApplication, QWidget, QPushButton, QMainWindow, QLabel, QFormLayout
 from PySide6.QtCore import Qt
 
 import sys
@@ -7,15 +7,29 @@ import sys
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
+
+
+        formulario =   QFormLayout()
+       
+        
         self.setWindowTitle("Exercicio2")
         self.button = QPushButton("butao", self)
         self.setFixedSize(300,300)
         self.button.setGeometry(100, 100 ,100 , 100)
         self.result_label = QLabel(self)
         self.result_label.setGeometry(10,90,280,30)
+
+        formulario.addRow(self.button)
+        formulario.addRow(self.result_label)
         
         self.button.clicked.connect(self.imprimir)
         
+        widget = QWidget()
+        widget.setLayout(formulario)
+
+        # Definindo o widget central da janela principal
+        self.setCentralWidget(widget)
+
     def imprimir(self):
         numero = 4
         if numero %2 == 0:
@@ -25,6 +39,7 @@ class MainWindow(QMainWindow):
 
 
 app = QApplication(sys.argv)
+app.setStyle("Fusion")
 janela = MainWindow()
 janela.show()
 app.exec()
