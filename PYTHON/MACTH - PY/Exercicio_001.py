68820ece1d30fe4d823e9aaa034e59ab9c13eae0
Gleison <gleisonsenac06@gmail.com>
2023-10-27 06:14:27 -0300

    Update Exercicios e mensagem automatica
    mandando pra nuvem as atualizações do pc de casa e subindo a messagem automatica

---
+++ PYTHON/MACTH - PY/Exercicio_001.py
@@ -0,0 +1,11 @@
+dia = 3
+
+match dia:
+    case 1:
+        print("BABABUI")
+    case 2:
+        print("BABABUI2")
+    case 3:
+        print("BABABUI3")
+    case _:
+        print("BABABUI CASE_")
