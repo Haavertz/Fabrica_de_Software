0ed6aa3693b183936f5caa2ff0f7f7e6d342e0ca
Gleison <gleisonsenac06@gmail.com>
2023-10-28 14:18:52 -0300

    Update Encaixando as pecas nos lugares
    tentando encaixar as pecas nos lugares determinados

--- PYTHON/EXERCICIOS - PY/Exercicio_09.py
+++ PYTHON/EXERCICIOS - PY/Exercicio_09.py
@@ -1,28 +1,35 @@
 class Organizar_Tabuleiro:
+    list_pecas = ["♖","♘"]
     def __init__(self):
-        self.Initial(0)
+        self.Initial()
 
-    def Initial(self, line="|_|"):
-        if line == "|_|":
-            return None
-        else:
-            for i in range(0, 9):
-                for j in range(0, 9):
-                    if i == 0 and j == 0:
-                        print(" +", "-" * 25, "+")
-                    print("", line, end=" ")
 
-                    if j == 8:
-                        print("", i, end="")
-                        print(end="\n")
+    def Initial(self):
+        for i in range(0, 9):
+            for j in range(0, 9):
+                if i == 0 and j == 0:
+                    print(" +", "-" * 25, "+")
+                    # for row in self.list_pecas:
+                    #     for col in row:    
+                    #         print("",col)
+                if j == 8:
+                    print("", i, end="")
+                    print(end="\n")
 
-                    if i == 8 and j == 8:
-                        alfabeto = ["a", "b", "c", "d", "e", "f", "g", "h", "i"]
-                        for describe in alfabeto:
-                            print("", describe, end=" ")
+                if i == 8 and j == 8:
+                    alfabeto = ["a", "b", "c", "d", "e", "f", "g", "h", "i"]
+                    for describe in alfabeto:
+                        print("", describe, end=" ")
 
-                        print("\n", "+", "-" * 25, "+", end="")
-                        print("\n")
+                    print("\n", "+", "-" * 25, "+", end="")
+                    print("\n")
+                    self.process_cell()
+
+    def process_cell(self, row="♖", col="♖"):
+        if row == "♖" and col == "♖":
+            print("♖")
+            
+
 
 
 class MovimentoTabuleiro(Organizar_Tabuleiro):
@@ -30,14 +37,9 @@ class MovimentoTabuleiro(Organizar_Tabuleiro):
         super().__init__()
         self.Movimento_Percorrer()
       
-
     def Movimento_Percorrer(self):
-        cont = 0
         for i in range(0, 9):
             for j in range(0, 9):
-                # if i == 0 or j == 0:
-                cont = cont+1
-                print(cont)                    
-
+                pass
 
 MovimentoTabuleiro()
