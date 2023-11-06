7198aaedf4a2cc1149de10c17c82e2007b9bbe8a
jotafreitas <130395809+Haavertz@users.noreply.github.com>
2023-11-06 17:20:55 -0400

    DesafioScreens

---
+++ KIVY/Exercicio10.py
@@ -0,0 +1,14 @@
+import kivy
+from kivy.app import App
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.button import Button
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.image import Image
+
+class Adivinhe(App):
+    def Build(self):
+        pass
+    
+if __name__ == "__main__":
+    Adivinhe().run()
