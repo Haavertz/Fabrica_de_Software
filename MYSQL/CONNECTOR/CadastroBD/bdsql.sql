8481841f8e03651a0ef9569ba4e1a22ba207f965
jotafreitas <130395809+Haavertz@users.noreply.github.com>
2023-11-16 16:24:03 -0400

    Update Ex

--- MYSQL/CONNECTOR/CadastroBD/bdsql.sql
+++ MYSQL/CONNECTOR/CadastroBD/bdsql.sql
@@ -5,13 +5,17 @@ create table cadastro(
 id int auto_increment primary key,
 nome varchar(75) not null,
 sobrenome varchar(75) not null,
-cpf varchar(11),
+cpf varchar(11) not null,
 email varchar(255) not null,
-telefone int default(0) not null,
+telefone varchar(12) not null,
 endereco varchar(255) not null,
 data_nascimento varchar(255) not null,
-complento varchar(10),
-cep varchar(8) not null
-senha varchar(255) not null,
+complemento varchar(10),
+cep varchar(8) not null,
+senha varchar(255) not null
 );
 
+describe cadastro;
+
+insert into cadastro (nome, sobrenome, cpf, email, telefone, endereco, data_nascimento, complemento, cep, senha) 
+value ("Gleison","Morais","12345678911","gleisonmorales@gmail.com","67991827644", "Rua do Pelicano", "25/07/2002","1210","79097021","123");
