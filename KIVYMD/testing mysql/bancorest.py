be2033d33b3b8cd119c25003029dd6526c31fc8c
Gleison <gleisonsenac06@gmail.com>
2023-12-02 08:41:44 -0300

    .

--- KIVYMD/testing mysql/bancorest.py
+++ KIVYMD/testing mysql/bancorest.py
@@ -1,19 +1,15 @@
 from kivymd.app import MDApp
 from kivymd.uix.label import MDLabel
-from kivymd.uix.button import MDRaisedButton
 from kivymd.uix.boxlayout import MDBoxLayout
 from kivymd.uix.screen import MDScreen
 from kivymd.uix.selectioncontrol import MDCheckbox
-from kivymd.app import MDApp
 from kivy.uix.screenmanager import ScreenManager, Screen
-
 
 class Tela_Principal(MDScreen):
     
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.orientation = 'vertical'
-
 
         layout = MDBoxLayout(orientation='vertical')
         
@@ -23,27 +19,38 @@ class Tela_Principal(MDScreen):
         label = MDLabel(
             text="Escolha seu serviço e seu horário:",
             halign="center",
-            size_hint=(None, None),
-            size=(200, 100),
-            pos_hint={'center_x': 0.5, 'center_y': 0.5},
-            line_color=(0, 0, 0, 1),
-            markup=True
+            size_hint=(None,None),
+            size=(250, 100),
+            pos_hint={'center_x':0.5,'center_y': 0.5},
+            padding = [0,0,0,0]
         )
 
         checkbox = MDCheckbox(
             size_hint=(None, None),
             size=(48, 48),
-            pos_hint={'center_y': 0.5} 
+            halign="center",
+            pos_hint={'center_y': 0.5},
+            padding = [0,0,0,0],
+            active = False
+        
         )
 
+        box_layout = MDBoxLayout(orientation='horizontal', padding=[0, 0, 0, 0], 
+                                 line_color=(0,0,0,1), size_hint=(None, None), 
+                                 size=(300, 100), pos_hint={'center_x':0.5,'center_y': -0.5})
 
-        frame = MDBoxLayout(orientation='vertical', size_hint_y=None, height=500, padding=[10, 10, 10, 10])
+        box_layout.add_widget(label)
+        box_layout.add_widget(checkbox)
+
+        frame = MDBoxLayout(orientation='vertical', size_hint_y=None, height=500)
+
+
+
         frame.md_bg_color = [1.0, 1.0, 1.0, 1.0] 
         frame.radius = [20, 20, 0, 0] 
-    
-        frame.add_widget(checkbox)
 
-        frame.add_widget(label)
+        frame.add_widget(box_layout)
+
         layout.add_widget(frame)
 
         self.add_widget(layout)
